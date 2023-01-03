4a3c9e8af119ea2d6a6f68ca3d9ef1a5cb5c02d7
Mahyuddin Susanto <udienz@gmail.com>
2023-01-03 14:40:15 +0700

    Improvement in Jenkins
     - Dockerfile: Change HOME dir to /opt/librenms.
     - Jenkinsfile: Add Jenkins CI.
     - scripts/*: Add scripts to publish the CI statuses

---
+++ scripts/inprogress.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set +x
+
+curl "${env.GITHUB_API_URL}/statuses/$GIT_COMMIT" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -H "Accept: application/vnd.github+json" \
+    -X POST \
+    -d "{\"state\": \"pending\",\"context\": \"continuous-integration/jenkins\", \"description\": \"Jenkins\", \"target_url\": \"$BUILD_URL\"}"
