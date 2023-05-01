9c6dea91cb2848873da7f257b37488447297e251
Mahyuddin Susanto <udienz@gmail.com>
2023-05-01 18:55:29 +0700

    Delete scripts directory

--- scripts/failure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-curl "$GITHUB_API_URL/statuses/$GIT_COMMIT" \
-              -H "Content-Type: application/json" \
-              -H "Authorization: token $GITHUB_TOKEN" \
-              -H "Accept: application/vnd.github+json" \
-              -X POST \
-              -d "{\"state\": \"failure\",\"context\": \"continuous-integration/jenkins\", \"description\": \"Jenkins\", \"target_url\": \"$BUILD_URL\"}"
